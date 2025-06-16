f1573bcfd9be0b64e43d6463f663bb3af0a02b73
Chambonini <137449949+Chambonini@users.noreply.github.com>
2025-06-16 21:30:33 +0000

    confirmo cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
+const who = ["A two headed pinecone", "My neighbor's llama", "An invisible clown", "The ghost of my ex"];
+const action = ["yelled at", "danced with", "threw spaghetti at", "sneezed on"];
+const what = ["my sandwich", "my homework", "my favorite mug", "the TV"];
+const where = ["in my house", "at the supermarket", "during a Zoom call", "in the bathtub"];
 
+// Generar excusa aleatoria
+function generateExcuse() {
+    const w = who[Math.floor(Math.random() * who.length)];
+    const a = action[Math.floor(Math.random() * action.length)];
+    const obj = what[Math.floor(Math.random() * what.length)];
+    const loc = where[Math.floor(Math.random() * where.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    return `${w} ${a} ${obj} ${loc}`;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Mostrar excusa en el HTML
+document.getElementById("excuse").innerText = generateExcuse();
